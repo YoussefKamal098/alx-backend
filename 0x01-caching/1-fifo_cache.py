9161c0befd9a72f210861de0a70bd5c4b8bb5853
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-30 19:49:10 +0300

    updated is_full method for FIFO cache class

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -17,19 +17,22 @@ class FIFOCache(BaseCaching):
         super().__init__()
         self.cache_data = OrderedDict()
 
+    def is_full(self):
+        """Checks if the cache is full.
+        """
+        return len(self.cache_data) >= BaseCaching.MAX_ITEMS
+
     def put(self, key, item):
         """Adds an item in the cache.
         """
         if key is None or item is None:
             return
 
-        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and
-                key not in self.cache_data):
+        if self.is_full() and key not in self.cache_data:
             last_key, _ = self.cache_data.popitem(last=False)
             print("DISCARD:", last_key)
 
         self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=False)
 
     def get(self, key):
         """Retrieves an item by key.
