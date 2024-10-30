ddaa32afc98bf6b6ab533dd3da50fc0bec9fe665
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-30 20:00:40 +0300

    updated FIFO cache class put method

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -33,6 +33,7 @@ class FIFOCache(BaseCaching):
             print("DISCARD:", last_key)
 
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
 
     def get(self, key):
         """Retrieves an item by key.
