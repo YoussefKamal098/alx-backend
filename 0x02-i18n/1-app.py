286a9940191f738119fa8022be14e46d6f446562
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-09 10:32:13 +0200

    flask babel 1-app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""A simple flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Setting the languages and the locale and timezone
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index() -> str:
+    """ Renders an index.html template """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
