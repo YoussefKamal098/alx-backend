7f40c6fdf48a8ff492b33c7a370ac575219b6efe
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-09 10:38:40 +0200

    updated flask babel 2-app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """ Renders an index.html template """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
